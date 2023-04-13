aa8c03cbf15f85db3a344024ddd464d295e9e185
giannischiout <giannis.chiout@gmail.com>
2023-04-13 14:03:49 +0300

    job slice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -38,6 +38,25 @@ export const loginUser = createAsyncThunk(
 })
 
 
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+  async (user, thunkAPI) => {
+    console.log('ThunkAPI')
+    console.log(thunkAPI.getState().user.user)
+    try {
+      const resp = await customFetch.patch('auth/updateUser', user, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        }
+      });
+      return resp.data;
+    } catch (error) {
+      console.log(error.response);
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+)
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -80,6 +99,21 @@ const userSlice = createSlice({
       toast.success(`Welcome back ${user.name}`);
     })
     .addCase(loginUser.rejected, (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    })
+    //Update User:
+    .addCase(updateUser.pending, (state, { payload }) => {
+      state.isLoading = true;
+    })
+    .addCase(updateUser.fulfilled, (state, { payload }) => {
+      const { user } = payload;
+      state.isLoading = false;
+      state.user = user;
+      addUserToLocalStorage(user)
+      toast.success(`User: ${user.name} updated`);
+    })
+    .addCase(updateUser.rejected, (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
     })
--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -1,6 +1,26 @@
 import React from 'react'
+import { FormRow } from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+
+
 
 const AddJob = () => {
+  const dispatch = useDispatch();
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    editJobId,
+  } = useSelector((store) => store.job);
+
   return (
     <div>AddJob</div>
   )
